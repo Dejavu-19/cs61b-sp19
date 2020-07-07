d2108b4fd4939d638d7270d3322ff20d694ae330
Dejavu <liukunling@yahoo.com>
2020-07-07 16:45:04 +0900

    test 2 proj0

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,11 +1,22 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Dejavu
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+
+
+    public static boolean isLeapYear(int year) {
+        if ((year % 400 == 0) || (year%4==0 && year%100!=0) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
@@ -30,4 +41,3 @@ public class LeapYear {
         }
     }
 }
-
